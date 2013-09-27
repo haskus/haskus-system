de48cc9e950150326aee11acba9a00badd4343f2
Sylvain HENRY <hsyl20@gmail.com>
2013-09-27 04:49:43 +0200

    Platform: allocate/release implemented for host

--- lib/ViperVM/Platform/Platform.hs
+++ lib/ViperVM/Platform/Platform.hs
@@ -1,39 +1,54 @@
-module ViperVM.Platform.Platform where
+{-# LANGUAGE ForeignFunctionInterface #-}
+module ViperVM.Platform.Platform (
+   Memory(..), Buffer(..), Result(..),
+   MemoryPeer(..), BufferPeer(..), AllocError(..),
+   allocate, release
+) where
 
-import Foreign.Ptr
+import Control.Applicative ( (<$>) )
 import Control.Concurrent.STM
+import Data.List
 import Data.Word
-import Foreign.Marshal.Alloc
+import Foreign.Ptr
+import Foreign.C.Types
 
 type ID = Int
 
 data Memory = Memory {
-	memoryId :: ID,
-	memoryPeer :: MemoryPeer,
-	memoryBuffers :: TVar [Buffer]
+   memoryId :: ID,
+   memoryPeer :: MemoryPeer,
+   memoryBuffers :: TVar [Buffer]
 }
+
+instance Eq Memory where
+   (==) a b = memoryId a == memoryId b
 
 data MemoryPeer =
-	  HostMemory
-	| CUDAMemory
-	| OpenCLMemory
-	| DiskMemory FilePath
+     HostMemory
+   | CUDAMemory
+   | OpenCLMemory
+   | DiskMemory
 
 data Buffer = Buffer {
-	bufferMemory :: Memory,
+   bufferMemory :: Memory,
    bufferSize :: BufferSize,
-	bufferPeer :: BufferPeer
-}
+   bufferPeer :: BufferPeer
+} deriving (Eq)
 
 data BufferPeer = 
-	  HostBuffer (Ptr ())
-	| CUDABuffer
-	| OpenCLBuffer
-	| DiskBuffer FilePath
+     HostBuffer (Ptr ())
+   | CUDABuffer
+   | OpenCLBuffer
+   | DiskBuffer
+   deriving (Eq)
 
 type BufferSize = Word64
 
 data Result a b = Success a | Error b
+
+onSuccess :: Result a b -> (a -> IO c) -> IO (Result c b)
+onSuccess (Success a) f = Success <$> f a
+onSuccess (Error b) _ = return (Error b)
 
 data AllocError = 
      ErrAllocOutOfMemory
@@ -44,24 +59,39 @@ allocate :: Word64 -> Memory -> IO (Result Buffer AllocError)
 allocate size mem = do
 
    bufPeer <- case (memoryPeer mem) of
-      HostMemory -> allocateHost size mem
-      CUDAMemory -> undefined
+      HostMemory   -> allocateHost size mem
+      CUDAMemory   -> undefined
       OpenCLMemory -> undefined
-      DiskMemory _ -> undefined
+      DiskMemory   -> undefined
 
-   case bufPeer of
-      Error err -> return (Error err)
-      Success peer -> do
-         let buf = Buffer mem size peer
-         atomically $ do
-            bufs <- readTVar (memoryBuffers mem)
-            writeTVar (memoryBuffers mem) (buf:bufs)
-         return (Success buf)
+   onSuccess bufPeer $ \peer -> do
+      let buf = Buffer mem size peer
+      atomically $ do
+         bufs <- readTVar (memoryBuffers mem)
+         writeTVar (memoryBuffers mem) (buf:bufs)
+      return buf
+
+foreign import ccall unsafe "stdlib.h malloc"  malloc :: CSize -> IO (Ptr a)
+foreign import ccall unsafe "stdlib.h free"    free   :: Ptr a -> IO ()
 
 -- | Allocate a buffer in host memory
 allocateHost :: Word64 -> Memory -> IO (Result BufferPeer AllocError)
 allocateHost size _ = do
-   ptr <- mallocBytes (fromIntegral size)
-   return $ if ptr == nullPtr 
-      then Error ErrAllocUnknown --TODO: detect out of memory
+   ptr <- malloc (fromIntegral size)
+   return $ if ptr == nullPtr
+      then Error ErrAllocOutOfMemory
       else Success (HostBuffer ptr)
+
+-- | Release a buffer
+release :: Buffer -> IO ()
+release buf = do
+   atomically $ do
+      let bufsVar = memoryBuffers (bufferMemory buf)
+      bufs <- readTVar bufsVar
+      writeTVar bufsVar (delete buf bufs)
+
+   case bufferPeer buf of
+      HostBuffer ptr -> free ptr
+      CUDABuffer     -> undefined
+      OpenCLBuffer   -> undefined
+      DiskBuffer     -> undefined
