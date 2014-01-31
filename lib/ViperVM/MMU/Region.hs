425bcbccb94488ead940ba68c38a2e2a39d377ea
Sylvain HENRY <hsyl20@gmail.com>
2014-01-31 15:35:36 +0100

    [MMU] Introduce Types and Regions
    Regions represent sets of bytes. Types describe the meaning of raw
    bytes. Currently we support scalars, arrays and basic structures.

---
+++ lib/ViperVM/MMU/Region.hs
@@ -0,0 +1,61 @@
+-- | A region is the shape of a set of memory cells in a buffer with an offset
+module ViperVM.MMU.Region(
+   Region(..), Offset,
+   regionOffset, regionsWithSameShape, overlapsAny, overlaps
+) where
+
+import Data.Word
+
+type Offset = Word64    -- ^ Offset in bytes
+type Size = Word64      -- ^ Size in bytes
+type Padding = Word64   -- ^ Padding (may be changed to a smaller type in the future)
+type RowCount = Word64  -- ^ Number of rows
+
+-- | Shape of a set of memory cells at a given offset
+data Region = 
+     Region1D Offset Size                    -- ^ Contiguous set of cells
+   | Region2D Offset RowCount Size Padding   -- ^ Rectangular set of cells
+   deriving (Eq,Ord,Show)
+
+-- | Check if two regions have the same shape (discarding offset and padding)
+regionsWithSameShape :: Region -> Region -> Bool
+regionsWithSameShape (Region1D _ s1) (Region1D _ s2) = s1 == s2
+regionsWithSameShape (Region2D _ idx1 s1 _) (Region2D _ idx2 s2 _) = (s1 == s2) && (idx1 == idx2)
+regionsWithSameShape _ _ = False
+
+-- | Return region offset in buffer
+regionOffset :: Region -> Offset
+regionOffset (Region1D off _) = off
+regionOffset (Region2D off _ _ _) = off
+
+
+-- | Retrieve regions that overlap with the given region
+overlapsAny :: Region -> [Region] -> [Region]
+overlapsAny reg = filter (overlaps reg)
+
+-- | Indicate if two regions overlap (may return false positive)
+overlaps :: Region -> Region -> Bool
+overlaps (Region1D off1 sz1) (Region1D off2 sz2) = not (left1 >= right2 || left2 >= right1)
+   where
+      left1 = off1
+      left2 = off2
+      right1 = off1+sz1
+      right2 = off2+sz2
+
+overlaps (Region1D off sz) r2 = overlaps (Region2D off 1 sz 0) r2
+overlaps r2 (Region1D off sz) = overlaps r2 (Region2D off 1 sz 0)
+
+overlaps (Region2D off1 rc1 sz1 pad1) (Region2D off2 rc2 sz2 pad2) | sz1+pad1 == sz2+pad2 = 
+   not ( left1 >= right2 || left2 >= right1 || top1 >= bottom2 || top2 >= bottom1 )
+   where
+      width = sz1+pad1
+      (top1,left1) = divMod off1 width
+      (top2,left2) = divMod off2 width
+      right1 = left1 + sz1
+      right2 = left2 + sz2
+      bottom1 = top1 + rc1
+      bottom2 = top2 + rc2
+
+-- FIXME: by default we say that 2 regions Region2D overlap if they don't have the same padding.
+-- Maybe we could improve this if we find a fast algorithm
+overlaps _ _ = True
---
+++ lib/ViperVM/MMU/Types.hs
@@ -0,0 +1,71 @@
+module ViperVM.MMU.Types (
+   Type(..), ScalarType(..), 
+   Endianness(..), Sign(..), IntBits(..),
+   SizeOf,
+   coarseRegionFromType
+) where
+
+import Data.Word
+import Control.Applicative ((<$>))
+import ViperVM.MMU.Region
+
+type ArraySize = Word64
+
+data Type = 
+     Scalar ScalarType
+   | Padding Word64
+   | Array Type ArraySize
+   | Struct [Type]
+
+data Endianness = LittleEndian | BigEndian
+data Sign = Signed | Unsigned
+data IntBits = Bit8 | Bit16 | Bit32 | Bit64
+
+data ScalarType =
+     TInt Sign IntBits Endianness
+   | TFloat Endianness
+   | TDouble Endianness
+
+
+class SizeOf t where
+   sizeOf :: t -> Word64
+
+instance SizeOf ScalarType where
+   sizeOf (TInt _ Bit8  _) = 1
+   sizeOf (TInt _ Bit16 _) = 2
+   sizeOf (TInt _ Bit32 _) = 4
+   sizeOf (TInt _ Bit64 _) = 8
+   sizeOf (TFloat {})      = 4
+   sizeOf (TDouble {})     = 8
+
+instance SizeOf Type where
+   sizeOf (Scalar x) = sizeOf x
+   sizeOf (Padding n) = n
+   sizeOf (Array t n) = n * sizeOf t
+   sizeOf (Struct ts) = foldl(+) 0 (sizeOf <$> ts)
+
+-- | Return coarse region containing effective data
+--
+-- Only the last padding bytes of structures in outermost arrays is deleted to
+-- build 2D regions
+coarseRegionFromType :: Type -> Offset -> Region
+coarseRegionFromType t off = case t of
+   Scalar x  -> Region1D off (sizeOf x)
+   Padding _ -> Region1D off 0
+   Array (Struct []) _ -> Region1D off 0
+   Array (Struct ts) n -> let (useful,padding) = stripStructPadding ts in
+      if padding == 0 
+         then Region1D off (n * useful)
+         else Region2D off n useful padding
+   Array t' n -> Region1D off (n * sizeOf t')
+   Struct ts -> Region1D off (fst $ stripStructPadding ts)
+
+-- | Return (useful,padding) where `padding` is the number
+-- of padding bytes at the end of the structure and `useful`
+-- the number of remaining bytes (useful ones and other padding)
+stripStructPadding :: [Type] -> (Word64,Word64)
+stripStructPadding ts = (useful,padding)
+   where
+      (useful,padding) = foldr f (0,0) ts
+      f (Padding p') (0,p) = (0,p'+p)
+      f t (u,p) = (u+sizeOf t, p)
