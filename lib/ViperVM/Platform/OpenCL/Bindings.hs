8ec3707a43857ecdf6399ba86089c0c65555238e
Sylvain HENRY <hsyl20@gmail.com>
2014-02-26 16:04:30 +0100

    [OpenCL] Fix CLSet.fromCLSet

--- lib/ViperVM/Platform/OpenCL/Bindings.hs
+++ lib/ViperVM/Platform/OpenCL/Bindings.hs
@@ -7,14 +7,14 @@ class Enum a => CLConstant a where
    toCL :: Integral b => a -> b
    fromCL :: Integral b => b -> a
 
-class (Bounded a, Enum a) => CLSet a where
+class Enum a => CLSet a where
    toCLSet :: (Bits b, Integral b) => [a] -> b
    toCLSet = sum . map f
       where f = shiftL 1 . fromIntegral . fromEnum
 
    fromCLSet :: (Bits b, Integral b) => b -> [a]
-   fromCLSet x = mapMaybe f [0..maxBound]
-      where f idx = if testBit x idx
-                        then Just (toEnum (idx+1)) 
+   fromCLSet x = mapMaybe f (enumFrom (toEnum 0))
+      where f e = if testBit x (fromEnum e)
+                        then Just e
                         else Nothing
 
