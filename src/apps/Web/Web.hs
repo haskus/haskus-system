bc95fe1c0d2a50576a841950b97b02cfdf0d2275
Sylvain HENRY <hsyl20@gmail.com>
2014-09-10 19:39:58 +0200

    List buffers per memory

--- src/apps/Web/Web.hs
+++ src/apps/Web/Web.hs
@@ -5,8 +5,11 @@ import ViperVM.Platform.Host as V
 import ViperVM.Platform.PlatformInfo
 import ViperVM.Platform.Loading
 import ViperVM.Platform.Config
+import ViperVM.Platform.Memory
+import ViperVM.Platform.Memory.Buffer
 import ViperVM.Platform.Topology as V
 import ViperVM.Arch.OpenCL.All as CL
+import qualified ViperVM.STM.TSet as TSet
 
 import Paths_ViperVM
 import Data.Version
@@ -15,11 +18,12 @@ import Control.Concurrent.STM
 import qualified Data.Set as Set
 
 import Control.Applicative ((<$>))
-import Control.Monad (msum, forM_)
+import Control.Monad (msum, forM_, guard)
 import Control.Monad.Trans.Class (lift)
 import System.Environment
 import Text.Printf
 import Network.Socket (withSocketsDo)
+import Data.Maybe (listToMaybe, isJust)
 
 import Happstack.Server
 
@@ -45,17 +49,24 @@ server :: Conf -> V.Host -> IO ()
 server conf pf = do
    putStrLn (printf "Starting Web server at localhost:%d" (port conf))
    simpleHTTP conf $ msum
-      [ dir "css" $ dir "style.css" $ do
+      [ 
+      
+        -- CSS 
+        dir "css" $ dir "style.css" $ do
          cssPath <- lift $ getDataFileName "data/web/css/style.css"
          serveFile (asContentType "text/css") cssPath
 
         -- Show platform information
-      , dir "platform" $ showPlatform pf
+      , dir "platform" $ dir "memory" $ path $ \uid -> showMemory pf uid
+
+        -- Show platform information
+      , dir "platform" $ showHost pf
 
         -- Show welcome screen
       , nullDir >> (ok . toResponse . appTemplate pf "Welcome" $ showWelcome)
       ]
 
+-- | Template of all pages
 appTemplate :: V.Host -> String -> Html -> Html
 appTemplate _ title bdy = docTypeHtml $ do
    H.head $ do
@@ -70,13 +81,15 @@ appTemplate _ title bdy = docTypeHtml $ do
          ! A.class_ "headtitle"
       bdy
 
+-- | Welcoming screen
 showWelcome :: Html
 showWelcome = do
    H.p "Welcome to ViperVM Web Interface"
    H.a "Platform information" ! A.href "/platform"
 
-showPlatform :: V.Host -> ServerPartT IO Response
-showPlatform pf = do
+-- | Show the host
+showHost :: V.Host -> ServerPartT IO Response
+showHost pf = do
 
    (mems,procs,nets) <- lift . atomically $ do
       let extractMem xs x = return (x:xs)
@@ -87,8 +100,9 @@ showPlatform pf = do
 
    ok . toResponse . appTemplate pf "Platform" $ do
       H.h2 "Memories"
-      H.ul $ forM_ mems $ \mem -> do
-         H.li . toHtml $ memoryInfo mem
+      H.ul $ forM_ mems $ \mem -> H.li $ do
+         H.a (toHtml $ memoryInfo mem)
+            ! A.href (H.toValue $ "/platform/memory/" ++ memoryUID mem)
 
       H.h2 "Processors"
       H.ul $ forM_ procs $ \p -> do
@@ -97,3 +111,28 @@ showPlatform pf = do
       H.h2 "Networks"
       H.ul $ forM_ nets $ \p -> do
          H.li . toHtml $ networkInfo p
+
+-- | Show a memory
+showMemory :: V.Host -> String -> ServerPartT IO Response
+showMemory pf uid = do
+   let extractMem xs x = return (x:xs)
+   mems <- lift $ atomically (foldMemories pf [] extractMem)
+
+   let mem = listToMaybe [x | x <- mems, memoryUID x == uid]
+
+   -- check that the memory with the given identifier exists
+   guard (isJust mem)
+
+   let Just m = mem
+   (nbuffers,bufferSizes) <- lift $ atomically $ do
+      n <- memoryBufferCount m
+      szs <- fmap bufferSize <$> (TSet.toList $ memoryBuffers m)
+      return (n,szs)
+   
+   ok . toResponse . appTemplate pf ("Memory - " ++ uid) $ do
+      H.h2 (toHtml $ "Memory" ++ uid)
+      H.h2 (toHtml (printf "Buffers (%d)" nbuffers :: String))
+
+      H.ul $ forM_ bufferSizes $ \sz -> do
+         let sizeMB = (fromIntegral sz / (1024.0 * 1024.0) :: Float)
+         H.li . toHtml $ (printf "Buffer - %f MB" sizeMB :: String)
