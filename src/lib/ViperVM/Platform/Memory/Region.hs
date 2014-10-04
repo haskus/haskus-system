bc9a6ebb6c285fef61896d006119744174ef9477
Sylvain HENRY <hsyl20@gmail.com>
2014-10-05 01:43:01 +0200

    Minor renaming

--- src/lib/ViperVM/Platform/Memory/Region.hs
+++ src/lib/ViperVM/Platform/Memory/Region.hs
@@ -16,9 +16,9 @@ module ViperVM.Platform.Memory.Region
    (
    -- * Shapes
      Shape(..)
-   , Size
+   , Width
+   , Height
    , Padding
-   , RowCount
    , shapeSimplify
    , shapeCover
    , isHomomorphicWith
@@ -37,19 +37,20 @@ import Data.Word
 import Control.Applicative ((<$>), (<*>))
 import Data.Maybe (isNothing)
 
--- | Size in bytes
-type Size = Word64      
+-- | Width in bytes
+type Width = Word64      
+
+-- | Number of rows
+type Height = Word64
    
 -- | Padding (may be changed to a smaller type in the future)
 type Padding = Word64
 
--- | Number of rows
-type RowCount = Word64
 
 -- | Shape of a set of memory cells at a given offset
 data Shape
-   = Shape1D Size                    -- ^ Contiguous set of cells
-   | Shape2D RowCount Size Padding   -- ^ Rectangular set of cells
+   = Shape1D Width                    -- ^ Contiguous set of cells
+   | Shape2D Height Width Padding   -- ^ Rectangular set of cells
    deriving (Eq,Ord,Show)
 
 -- | Positioned region (i.e. region shape with an offset)
@@ -59,24 +60,24 @@ data Region = Region
    }
 
 -- | Pattern synonym for region with 1D shape
-pattern Region1D off sz = Region off (Shape1D sz)
+pattern Region1D o w = Region o (Shape1D w)
 
 -- | Pattern synonym for region with 2D shape
-pattern Region2D off nrows sz padding = Region off (Shape2D nrows sz padding)
+pattern Region2D o h w p = Region o (Shape2D h w p)
 
 
 -- | Check if two regions have the same shape (discarding padding)
 isHomomorphicWith :: Shape -> Shape -> Bool
 isHomomorphicWith r1 r2 = case (shapeSimplify r1, shapeSimplify r2) of
-   (Shape1D s1, Shape1D s2)               -> s1 == s2
-   (Shape2D idx1 s1 _, Shape2D idx2 s2 _) -> (s1 == s2) && (idx1 == idx2)
-   _                                      -> False
+   (Shape1D w1, Shape1D w2)           -> w1 == w2
+   (Shape2D h1 w1 _, Shape2D h2 w2 _) -> (w1 == w2) && (h1 == h2)
+   _                                  -> False
 
 -- | Try to simplify the shape (e.g. transform a 2D region into a 1D one)
 shapeSimplify :: Shape -> Shape
-shapeSimplify (Shape2D idx s 0) = Shape1D (idx*s)
-shapeSimplify (Shape2D 1 s _)   = Shape1D s
-shapeSimplify r                 = r
+shapeSimplify (Shape2D h w 0) = Shape1D (h*w)
+shapeSimplify (Shape2D 1 w _) = Shape1D w
+shapeSimplify r               = r
 
 -- | Return covering 1D shape
 shapeCover :: Shape -> Shape
@@ -107,14 +108,14 @@ overlaps :: Region -> Region -> Bool
 overlaps r1 r2 =
    case (r1,r2) of
       -- Two 1D regions
-      (Region1D off1 sz1, Region1D off2 sz2) -> not (off1 >= off2+sz2 || off2 >= off1+sz1)
+      (Region1D o1 w1, Region1D o2 w2) -> not (o1 >= o2+w2 || o2 >= o1+w1)
 
       -- Try with covering 1D regions, if they do not overlap, r1 and r2 neither do
       _ | isNothing (regionCoverIntersection r1 r2) -> False
 
       -- Transform the 1D region into a 2D one
-      (Region1D off sz,_)     -> overlaps (Region2D off 1 sz 0) r2
-      (_, Region1D off sz)    -> overlaps r1 (Region2D off 1 sz 0)
+      (Region1D o w,_)     -> overlaps (Region2D o 1 w 0) r2
+      (_, Region1D o w)    -> overlaps r1 (Region2D o 1 w 0)
 
       -- 2D regions case
       -- FIXME: add test on h1,h2,o1,o2 if any (uncurry overlaps) rs is true
