1c542c95a90c5c09436caeb5dc3a41c8d06ddc5b
Sylvain HENRY <hsyl20@gmail.com>
2014-10-26 00:21:25 +0200

    Add comments

--- src/lib/ViperVM/STM/TGraph.hs
+++ src/lib/ViperVM/STM/TGraph.hs
@@ -19,11 +19,16 @@ deepFirst :: (Monad m, Ord a, Eq a) => (a -> m ()) -> (a -> m ()) -> (a -> m [a]
 deepFirst before after children xs = foldM_ go Set.empty xs
    where
       go visited x 
-         | Set.member x visited = return visited
+         | Set.member x visited = 
+            -- the node is already visited
+            return visited
+
          | otherwise = do
             before x
             cs <- children x
+            -- add current node to the visited ones to avoid "loops"
             let visited' = Set.insert x visited
+            -- visited "children" nodes
             visited'' <- foldM go visited' cs
             after x
             return visited''
@@ -36,11 +41,20 @@ deepFirst before after children xs = foldM_ go Set.empty xs
 breadthFirst :: (Monad m, Ord a, Eq a) => (a -> m Bool) -> (a -> m [a]) -> [a] -> m ()
 breadthFirst visit children = go Set.empty
    where
-      go _ [] = return ()
+      go _ [] = 
+         -- there are no more nodes to visit
+         return ()
+
       go visited (x:xs) 
-         | Set.member x visited = go visited xs
+         | Set.member x visited = 
+            -- the node is already visited, we skip it
+            go visited xs
+
          | otherwise = do
             b <- visit x
+            -- if "visit" returns False, we stop the traversal
             when b $ do
+               -- otherwise we add children to the list of nodes to
+               -- visit and we continue the traversal
                cs <- children x
                go (Set.insert x visited) (xs ++ cs)
