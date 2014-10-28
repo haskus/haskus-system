b0075a09cd82e9679759ab1acb87b79187ceac0d
Sylvain HENRY <hsyl20@gmail.com>
2014-10-28 18:42:28 +0100

    Remove memory manager

--- src/apps/MemoryTest.hs
+++ src/apps/MemoryTest.hs
@@ -2,17 +2,15 @@ import Control.Monad (forM)
 import Control.Applicative ((<$>))
 import Control.Concurrent.STM
 import Data.Foldable (traverse_,forM_)
-import qualified Data.Map as Map
 
 import ViperVM.Platform.Host
 import ViperVM.Platform.Config
 import ViperVM.Platform.Loading
 import ViperVM.Platform.Memory.Layout
-import ViperVM.Platform.Memory.Manager
+import ViperVM.Platform.Memory.Data
 import ViperVM.Platform.Topology
 import ViperVM.Platform.Transfer
 import ViperVM.Platform.TransferBench
-import ViperVM.STM.TMap as TMap
 import qualified ViperVM.STM.TSet as TSet
 
 main :: IO ()
@@ -25,23 +23,19 @@ main = do
    let extractMem xs x = return (x:xs)
    mems <- reverse <$> atomically (breadthFirstMemories pf [] extractMem)
 
-   putStrLn "\nCreate basic memory manager for each memory"
-   mgrs <- forM mems (initManager defaultManagerConfig)
-   let memManagers = Map.fromList (mems `zip` mgrs)
-
    putStrLn "============================================"
    putStrLn "Testing allocation/release..."
 
    putStrLn "\nAllocating data in each memory"
-   datas <- forM mgrs $ \mgr -> do
+   datas <- forM mems $ \mem -> do
       let 
-         dt endian = Array (Scalar (DoubleField endian)) (200*1024*1024)
+         layout endian = Array (Scalar (DoubleField endian)) (200*1024*1024)
          f = either (error . ("Allocation error: " ++) . show) id
       
-      f <$> allocateDataWithEndianness dt mgr
+      f <$> allocateDataWithEndianness layout mem
 
    putStrLn "\nReleasing data in each memory"
-   traverse_ (uncurry releaseData) (mgrs `zip` datas)
+   traverse_ releaseData datas
 
 
    putStrLn "============================================"
@@ -54,23 +48,19 @@ main = do
          putStrLn "\n  - Allocating data in 2 memories"
          let 
             dt endian = Array (Scalar (DoubleField endian)) 128
-            mgr1 = memManagers Map.! m1
-            mgr2 = memManagers Map.! m2
-            f = either (error . ("Allocation error: " ++) . show) id
-         d1 <- allocateDataWithEndianness dt mgr1
-         d2 <- allocateDataWithEndianness dt mgr2
-         b1 <- atomically $ managerData mgr1 TMap.! f d1
-         b2 <- atomically $ managerData mgr2 TMap.! f d2
+            getOrFail = either (error . ("Allocation error: " ++) . show) id
+         d1 <- getOrFail <$> allocateDataWithEndianness dt m1
+         d2 <- getOrFail <$> allocateDataWithEndianness dt m2
 
          putStrLn "  - Transferring between memories..."
-         (tr,duration) <- benchStr $ networkTransferData net b1 b2
+         (tr,duration) <- benchStr $ networkTransferData net d1 d2
          putStrLn "  - Waiting for transfer to end..."
          res <- atomically $ takeTMVar (transferResult tr)
          putStrLn ("  - Transfer result: " ++ show res ++ " duration: " ++ duration ++ " secs")
 
          putStrLn "  - Releasing data"
-         releaseData mgr1 (f d1)
-         releaseData mgr2 (f d2)
+         releaseData d1
+         releaseData d2
    
    putStrLn "Done."
 
--- src/lib/ViperVM/Platform/Memory/Data.hs
+++ src/lib/ViperVM/Platform/Memory/Data.hs
@@ -2,10 +2,16 @@
 module ViperVM.Platform.Memory.Data 
    ( dataCoveringRegion
    , dataCoveringRegion1D
+   , allocateData
+   , allocateDataWithEndianness
+   , releaseData
    )
 where
 
-import ViperVM.Platform.Types (Data(..))
+import ViperVM.Platform.Types (Memory(..),Data(..))
+import ViperVM.Arch.Common.Errors (AllocError(..))
+import ViperVM.Arch.Common.Endianness (Endianness(..))
+import ViperVM.Platform.Memory
 import ViperVM.Platform.Memory.Layout
 import ViperVM.Platform.Memory.Region
 
@@ -21,3 +27,28 @@ dataCoveringRegion d = Region (dataOffset d) (dataCoveringShape d)
 -- | Return the smallest covering 1D region
 dataCoveringRegion1D :: Data -> Region
 dataCoveringRegion1D = regionCover1D . dataCoveringRegion
+
+
+-- | Allocate a data in a newly allocated buffer in memory
+allocateData :: Layout -> Memory -> IO (Either AllocError Data)
+allocateData fm mem = do
+   buf <- memoryBufferAllocate (sizeOf fm) mem
+   --TODO: associate data with buffer?
+   return $ case buf of
+      Left err -> Left err
+      Right b  -> Right (Data b 0 fm)
+
+-- | Allocate a data in a newly allocated data in memory, passing endianness as a parameter for layout definition
+allocateDataWithEndianness :: (Endianness -> Layout) -> Memory -> IO (Either AllocError Data)
+allocateDataWithEndianness f mem = 
+   allocateData (f $ memoryEndianness mem) mem
+
+
+-- | Release a data
+releaseData :: Data -> IO ()
+releaseData d = do
+   let b = dataBuffer d
+
+   --TODO: disassociate data from buffer?
+   --TODO: check if we have to/can release the buffer?
+   memoryBufferRelease b
