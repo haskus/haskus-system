ff70dcd28b90517303fd4243401978cc5a2588e5
Sylvain HENRY <hsyl20@gmail.com>
2014-12-24 14:01:01 +0100

    [Linux] Test getConnectorCRTC

--- src/apps/TestLinuxGraphics.hs
+++ src/apps/TestLinuxGraphics.hs
@@ -5,6 +5,7 @@ import Control.Monad.Trans.Either
 import Control.Applicative ((<$>))
 import Control.Monad.IO.Class (liftIO)
 import Data.Foldable (forM_)
+import Data.Traversable (forM)
 
 main :: IO ()
 main = do
@@ -18,26 +19,52 @@ main = do
       _ <- (/= 0) <$> (EitherT $ getCapability ioctl fd CapDumbBuffer)
       liftIO $ putStrLn "The card has DumbBuffer capability :)"
 
-      -- Get resources
       liftIO $ putStrLn "==================\n= RESOURCES\n=================="
+
       res <- EitherT $ getModeResources ioctl fd
       liftIO $ putStrLn $ show res
 
+
       liftIO $ putStrLn "==================\n= CONNECTORS\n=================="
 
-      forM_ (connectors res) $ \connId -> do
+      conns <- forM (connectors res) $ \connId -> do
          conn <- EitherT $ getConnector ioctl fd connId
-
          liftIO $ putStrLn $ show conn
+         return conn
+
 
       liftIO $ putStrLn "==================\n= ENCODERS \n=================="
+
       forM_ (encoders res) $ \encId -> do
          enc <- EitherT $ getEncoder ioctl fd encId
          liftIO $ do
             putStrLn $ show enc
             putStrLn $ "Valid CRTCs: " ++ (show $ getEncoderCRTCs res enc)
 
+      liftIO $ putStrLn "==================\n= Test \n=================="
+      
+      -- Find connected connectors
+      let
+         isValid x  = connConnection x == Connected
+                      && not (null $ connModes x)
+         validConns = filter isValid conns
 
-   case ret of
-      Left err -> putStrLn $ "Error: " ++ show err
-      Right _ -> return ()
+         -- select first connector
+         conn = head validConns
+
+         -- select highest mode
+         mode = head (connModes conn)
+
+      -- check if the connector already has an associated encoder+crtc to avoid modesetting
+      (curCrtc,curEnc) <- EitherT $ getConnectorCRTC ioctl fd conn
+
+      liftIO $ putStrLn $ "Current CRTC and encoder: " ++ show (curCrtc,curEnc)
+
+         -- find a crtc
+         -- TODO
+
+         -- create a framebuffer
+         -- TODO
+
+
+   return ()
