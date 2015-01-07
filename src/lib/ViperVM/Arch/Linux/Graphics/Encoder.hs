d419950e78d6b57dac79f0fae0bc56d811906fa7
Sylvain HENRY <hsyl20@gmail.com>
2015-01-07 08:29:01 +0100

    [Linux] Support encoder types

--- src/lib/ViperVM/Arch/Linux/Graphics/Encoder.hs
+++ src/lib/ViperVM/Arch/Linux/Graphics/Encoder.hs
@@ -19,7 +19,14 @@ import ViperVM.Arch.Linux.FileDescriptor
 import ViperVM.Arch.Linux.Graphics.IDs
 
 
-newtype EncoderType = EncoderType Word32 deriving (Storable,Show)
+-- | Type of the encoder
+data EncoderType
+   = EncoderTypeNone
+   | EncoderTypeDAC
+   | EncoderTypeTMDS
+   | EncoderTypeLVDS
+   | EncoderTypeTVDAC
+   deriving (Eq,Ord,Show,Enum)
 
 data Encoder = Encoder
    { encoderID                   :: EncoderID
@@ -39,23 +46,29 @@ instance Storable Encoder where
       Encoder
          <$> peekByteOff ptr 0
 
-         <*> (EncoderType <$> peekByteOff ptr 4)
+         <*> (toEnum' <$> peekByteOff ptr 4)
          <*> (fmap ControllerID . wrapZero <$> peekByteOff ptr 8)
          <*> peekByteOff ptr 12
          <*> peekByteOff ptr 16
+      where
+         toEnum' :: Enum a => Word32 -> a
+         toEnum' = toEnum . fromIntegral
 
    poke ptr (Encoder {..}) = do
       pokeByteOff ptr 0 encoderID
-      pokeByteOff ptr 4 encoderType
+      pokeByteOff ptr 4 (fromEnum' encoderType)
       pokeByteOff ptr 8 (fromMaybe (ControllerID 0) encoderControllerID)
       pokeByteOff ptr 12 encoderPossibleControllers
       pokeByteOff ptr 16 encoderPossibleConnectors
+      where
+         fromEnum' :: Enum a => a -> Word32
+         fromEnum' = fromIntegral . fromEnum
 
 -- | Get encoder
 getEncoder :: IOCTL -> FileDescriptor -> EncoderID -> SysRet Encoder
 getEncoder ioctl fd encId = do
    
-   let res = Encoder encId (EncoderType 0) Nothing 0 0
+   let res = Encoder encId EncoderTypeNone Nothing 0 0
 
    ioctlReadWrite ioctl 0x64 0xA6 defaultCheck fd res
 
