ec88fbbec5f4e43be60895bd7688b5dbd43d050f
Sylvain HENRY <hsyl20@gmail.com>
2015-01-28 15:37:43 +0100

    [Linux] Add socket syscall

--- src/lib/ViperVM/Arch/X86_64/Linux/Network.hs
+++ src/lib/ViperVM/Arch/X86_64/Linux/Network.hs
@@ -2,6 +2,13 @@ module ViperVM.Arch.X86_64.Linux.Network
    ( sysShutdown
    , sysSendFile
    , sysSendFileWithOffset
+   , SocketProtocol(..)
+   , SocketRawType(..)
+   , SocketType(..)
+   , NetlinkType(..)
+   , IPType(..)
+   , sysSocket'
+   , sysSocket
    )
 where
 
@@ -9,13 +16,15 @@ import Foreign.Ptr (nullPtr)
 import Foreign.Marshal.Utils (with)
 import Foreign.Storable (peek)
 import Data.Word
+import Data.List (foldl')
+import Data.Bits
 
 import ViperVM.Arch.Linux.ErrorCode
 import ViperVM.Arch.Linux.FileDescriptor
 import ViperVM.Arch.X86_64.Linux.Syscall
 
-data ShutFlag =
-     ShutRead
+data ShutFlag
+   = ShutRead
    | ShutWrite
    | ShutReadWrite
    deriving (Enum,Show)
@@ -37,3 +46,195 @@ sysSendFileWithOffset (FileDescriptor outfd) (FileDescriptor infd) offset count 
       onSuccessIO (syscall4 40 outfd infd off count) $ \x -> do
          newOff <- peek off
          return (fromIntegral x, newOff)
+
+-- | Socket protocol family
+data SocketProtocol
+   = SockProtUNSPEC     -- ^ Unspecified (0)
+   | SockProtLOCAL      -- ^ Local to host (pipes and file-domain). Also called UNIX or FILE
+   | SockProtIPv4       -- ^ IP protocol family
+   | SockProtAX25       -- ^ Amateur Radio AX.25
+   | SockProtIPX        -- ^ Novell Internet Protocol
+   | SockProtAPPLETALK  -- ^ Appletalk DDP
+   | SockProtNETROM     -- ^ Amateur radio NetROM
+   | SockProtBRIDGE     -- ^ Multiprotocol bridge
+   | SockProtATMPVC     -- ^ ATM PVCs
+   | SockProtX25        -- ^ Reserved for X.25 project
+   | SockProtIPv6       -- ^ IP version 6
+   | SockProtROSE       -- ^ Amateur Radio X.25 PLP
+   | SockProtDECnet     -- ^ Reserved for DECnet project
+   | SockProtNETBEUI    -- ^ Reserved for 802.2LLC project
+   | SockProtSECURITY   -- ^ Security callback pseudo AF
+   | SockProtKEY        -- ^ PF_KEY key management API
+   | SockProtNETLINK    -- ^ Netlink. Also called ROUTE
+   | SockProtPACKET     -- ^ Packet family
+   | SockProtASH        -- ^ Ash
+   | SockProtECONET     -- ^ Acorn Econet
+   | SockProtATMSVC     -- ^ ATM SVCs
+   | SockProtRDS        -- ^ RDS sockets
+   | SockProtSNA        -- ^ Linux SNA Project
+   | SockProtIRDA       -- ^ IRDA sockets
+   | SockProtPPPOX      -- ^ PPPoX sockets
+   | SockProtWANPIPE    -- ^ Wanpipe API sockets
+   | SockProtLLC        -- ^ Linux LLC
+   | SockProt27         -- ^ Unknown. Added to simplify Enum deriving
+   | SockProt28         -- ^ Unknown. Added to simplify Enum deriving
+   | SockProtCAN        -- ^ Controller Area Network
+   | SockProtTIPC       -- ^ TIPC sockets
+   | SockProtBLUETOOTH  -- ^ Bluetooth sockets
+   | SockProtIUCV       -- ^ IUCV sockets
+   | SockProtRXRPC      -- ^ RxRPC sockets
+   | SockProtISDN       -- ^ mISDN sockets
+   | SockProtPHONET     -- ^ Phonet sockets
+   | SockProtIEEE802154 -- ^ IEEE 802.15.4 sockets
+   | SockProtCAIF       -- ^ CAIF sockets
+   | SockProtALG        -- ^ Algorithm sockets
+   | SockProtNFC        -- ^ NFC sockets
+   | SockProtVSOCK      -- ^ vSockets
+   deriving (Enum, Eq, Show)
+
+data SocketRawType
+   = SockRawTypeStream     -- ^ Sequenced, reliable, connection-based byte streams
+   | SockRawTypeDatagram   -- ^ Connectionless, unreliable datagrams of fixed maximum length
+   | SockRawTypeRaw        -- ^ Raw protocol interface
+   | SockRawTypeRDM        -- ^ Reliably delivered messages
+   | SockRawTypeSeqPacket  -- ^ Sequenced, reliable, connection-based, datagrams of fixed maximum length
+   | SockRawTypeDCCP       -- ^ Datagram congestion control protocol
+   | SockRawTypePacket     -- ^ Linux specific way of getting packets at the dev
+                           --   level. For writing rarp and other similar things on the user level
+   deriving (Show,Eq)
+
+instance Enum SocketRawType where
+   toEnum x = case x of
+      1  -> SockRawTypeStream
+      2  -> SockRawTypeDatagram
+      3  -> SockRawTypeRaw
+      4  -> SockRawTypeRDM
+      5  -> SockRawTypeSeqPacket
+      6  -> SockRawTypeDCCP
+      10 -> SockRawTypePacket
+      _  -> error "Invalid socket type"
+
+   fromEnum x = case x of
+      SockRawTypeStream    -> 1
+      SockRawTypeDatagram  -> 2
+      SockRawTypeRaw       -> 3
+      SockRawTypeRDM       -> 4
+      SockRawTypeSeqPacket -> 5
+      SockRawTypeDCCP      -> 6
+      SockRawTypePacket    -> 10
+
+-- | Socket option
+data SocketOption
+   = SockOptCloseOnExec    -- ^ Atomically set close-on-exec flag
+   | SockOptNonBlocking    -- ^ Atomically mark descriptor as non-blocking
+   deriving (Show,Eq)
+
+instance Enum SocketOption where
+   toEnum 0x80000 = SockOptCloseOnExec
+   toEnum 0x800   = SockOptNonBlocking
+   toEnum _       = error "Invalid socket option"
+
+   fromEnum SockOptCloseOnExec = 0x80000
+   fromEnum SockOptNonBlocking = 0x800
+
+-- | Create a socket (low-level API)
+--
+-- `subprotocol` may be 0
+sysSocket' :: SocketRawType -> SocketProtocol -> Int -> [SocketOption] -> SysRet FileDescriptor
+sysSocket' typ protocol subprotocol opts =
+   let
+      f :: Enum a => a -> Word64
+      f = fromIntegral . fromEnum
+      typ' = f typ .|. foldl' (\x y -> x .|. f y) 0 opts
+   in
+   onSuccess (syscall3 41 (fromEnum protocol) typ' subprotocol) (FileDescriptor . fromIntegral)
+
+-- | IP type
+data IPType
+   = IPv4
+   | IPv6
+   deriving (Show,Eq)
+
+-- | Netlink socket type
+data NetlinkType
+   = NetlinkTypeRoute            -- ^ Routing/device hook
+   | NetlinkTypeUserSocket       -- ^ Reserved for user mode socket protocols
+   | NetlinkTypeSocketDiagnostic -- ^ socket monitoring
+   | NetlinkTypeNFLOG            -- ^ netfilter/iptables ULOG
+   | NetlinkTypeXFRM             -- ^ ipsec
+   | NetlinkTypeSELINUX          -- ^ SELinux event notifications
+   | NetlinkTypeISCSI            -- ^ Open-iSCSI
+   | NetlinkTypeAudit            -- ^ auditing
+   | NetlinkTypeFibLookup
+   | NetlinkTypeConnector
+   | NetlinkTypeNetfilter        -- ^ netfilter subsystem
+   | NetlinkTypeIP6FW
+   | NetlinkTypeDNRTMSG          -- ^ DECnet routing messages
+   | NetlinkTypeKernelEvent      -- ^ Kernel messages to userspace
+   | NetlinkTypeGeneric
+   | NetlinkTypeSCSITransport    -- ^ SCSI Transports
+   | NetlinkTypeECryptFS
+   | NetlinkTypeRDMA
+   | NetlinkTypeCrypto           -- ^ Crypto layer
+   deriving (Show,Eq)
+
+instance Enum NetlinkType where
+   toEnum x = case x of
+      0  -> NetlinkTypeRoute
+      2  -> NetlinkTypeUserSocket
+      4  -> NetlinkTypeSocketDiagnostic
+      5  -> NetlinkTypeNFLOG
+      6  -> NetlinkTypeXFRM
+      7  -> NetlinkTypeSELINUX
+      8  -> NetlinkTypeISCSI
+      9  -> NetlinkTypeAudit
+      10 -> NetlinkTypeFibLookup
+      11 -> NetlinkTypeConnector
+      12 -> NetlinkTypeNetfilter
+      13 -> NetlinkTypeIP6FW
+      14 -> NetlinkTypeDNRTMSG
+      15 -> NetlinkTypeKernelEvent
+      16 -> NetlinkTypeGeneric
+      18 -> NetlinkTypeSCSITransport
+      19 -> NetlinkTypeECryptFS
+      20 -> NetlinkTypeRDMA
+      21 -> NetlinkTypeCrypto
+      _  -> error "Invalid NetLink type"
+
+   fromEnum x = case x of
+      NetlinkTypeRoute              -> 0
+      NetlinkTypeUserSocket         -> 2
+      NetlinkTypeSocketDiagnostic   -> 4
+      NetlinkTypeNFLOG              -> 5
+      NetlinkTypeXFRM               -> 6
+      NetlinkTypeSELINUX            -> 7
+      NetlinkTypeISCSI              -> 8
+      NetlinkTypeAudit              -> 9
+      NetlinkTypeFibLookup          -> 10
+      NetlinkTypeConnector          -> 11
+      NetlinkTypeNetfilter          -> 12
+      NetlinkTypeIP6FW              -> 13
+      NetlinkTypeDNRTMSG            -> 14
+      NetlinkTypeKernelEvent        -> 15
+      NetlinkTypeGeneric            -> 16
+      NetlinkTypeSCSITransport      -> 18
+      NetlinkTypeECryptFS           -> 19
+      NetlinkTypeRDMA               -> 20
+      NetlinkTypeCrypto             -> 21
+
+-- | Socket type
+data SocketType
+   = SockTypeTCP IPType
+   | SockTypeUDP IPType
+   | SockTypeNetlink NetlinkType
+   deriving (Show,Eq)
+
+-- | Create a socket
+sysSocket :: SocketType -> [SocketOption] -> SysRet FileDescriptor
+sysSocket typ opts =
+   case typ of
+      SockTypeTCP IPv4   -> sysSocket' SockRawTypeStream SockProtIPv4 0 opts
+      SockTypeTCP IPv6   -> sysSocket' SockRawTypeStream SockProtIPv6 0 opts
+      SockTypeUDP IPv4   -> sysSocket' SockRawTypeDatagram SockProtIPv4 0 opts
+      SockTypeUDP IPv6   -> sysSocket' SockRawTypeDatagram SockProtIPv6 0 opts
+      SockTypeNetlink nt -> sysSocket' SockRawTypeDatagram SockProtNETLINK (fromEnum nt) opts
