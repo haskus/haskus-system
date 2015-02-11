331e25e98e62f579d5a3aa20eb3aa5bd82787a6e
Sylvain HENRY <hsyl20@gmail.com>
2015-02-11 19:16:29 +0100

    Wait for user input before power-off

--- src/apps/OS/HaskOS.hs
+++ src/apps/OS/HaskOS.hs
@@ -1,11 +1,27 @@
 import ViperVM.Arch.X86_64.Linux.Power
-
-import Control.Monad (void)
+import ViperVM.Arch.X86_64.Linux.FileSystem
+import ViperVM.Arch.Linux.ErrorCode
 
 main :: IO ()
 main = do
 
    putStrLn "Booting HaskOS"
 
+   putStrLn "Press a key to continue"
+   waitKey
+
    putStrLn "And now, shutting down"
-   void $ sysPower PowerOff
+   check $ sysSync
+   check $ sysPower PowerOff
+
+
+waitKey :: IO ()
+waitKey = getChar >> return ()
+
+check :: SysRet a -> IO a
+check f = do
+   r <- f
+   case r of
+      Left err -> do
+         error (show err)
+      Right v -> return v
