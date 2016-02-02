c7b69c095b6b1a016df3944c836139efec0b97ec
Sylvain HENRY <hsyl20@gmail.com>
2016-02-03 00:54:30 +0100

    Only use input events

--- src/lib/ViperVM/System/Input.hs
+++ src/lib/ViperVM/System/Input.hs
@@ -13,6 +13,14 @@ import ViperVM.Arch.Linux.Input.Device as Input
 
 import Data.Traversable (forM)
 
+import Prelude hiding (init,tail)
+import Control.Monad (void)
+import Data.Maybe (isJust)
+
+import Text.Megaparsec
+import Text.Megaparsec.Lexer hiding (space)
+
+
 data InputDevice = InputDevice
    { inputDevicePath             :: FilePath          -- ^ SysFS path
    , inputDeviceDev              :: Device            -- ^ Device ID
@@ -23,7 +31,10 @@ data InputDevice = InputDevice
 
 loadInputDevices :: System -> Sys [InputDevice]
 loadInputDevices system = sysLogSequence "Load input devices" $ do
-   devs <- listDevicesWithClass system "input" (const True)
+   let
+      parseInput = void (string "event" >> decimal)
+      isInput    = isJust . parseMaybe parseInput
+   devs <- listDevicesWithClass system "input" isInput
    forM devs $ \(devpath,dev) -> do
       fd   <- openDevice system CharDevice dev
       name <- sysCallAssert "Get device name" $
