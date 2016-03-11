9bb255fe4ad85467881765133f2ee39225b2442b
Sylvain HENRY <hsyl20@gmail.com>
2016-03-11 21:19:52 +0100

    Support Framebuffer reuse

--- src/lib/ViperVM/Arch/Linux/Graphics/Helper.hs
+++ src/lib/ViperVM/Arch/Linux/Graphics/Helper.hs
@@ -1,25 +1,39 @@
 module ViperVM.Arch.Linux.Graphics.Helper
-   ( cardSetController
-   , cardSwitchFrameBuffer
+   ( FBConfig (..)
+   , setController
+   , switchFrameBuffer
    )
 where
 
-import ViperVM.Arch.Linux.Graphics.Mode
 import ViperVM.Arch.Linux.Graphics.Card
+import ViperVM.Arch.Linux.Graphics.Mode
 import ViperVM.Arch.Linux.Graphics.Controller
 import ViperVM.Arch.Linux.Graphics.Connector
 import ViperVM.Arch.Linux.Graphics.FrameBuffer
 import ViperVM.Arch.Linux.ErrorCode
 
+data FBConfig
+   = SetFB FrameBuffer
+   | ReuseFB
+   | ReleaseFB
+   deriving (Show)
+
 -- | Configure a controller
-cardSetController :: Card -> Controller -> Maybe FrameBuffer -> [Connector] -> Maybe Mode -> SysRet ()
-cardSetController card ctrl fb conns mode = do
-   let fbpos = fmap (\z -> FrameBufferPos (fbID z) 0 0) fb
+--
+-- A connected framebuffer is required to set a mode: if ReuseFB is passed, the
+-- connected one is used.
+setController :: Controller -> FBConfig -> [Connector] -> Maybe Mode -> SysRet ()
+setController ctrl fbconf conns mode = do
+   let 
+      fbpos = case fbconf of
+         SetFB fb  -> Just $ FrameBufferPos (fbID fb) 0 0
+         ReuseFB   -> Just $ FrameBufferPos (FrameBufferID (-1)) 0 0
+         ReleaseFB -> Nothing
+      card  = controllerCard ctrl
    setController' card (controllerID ctrl) fbpos (fmap connectorID conns) mode
 
 -- | Switch to another framebuffer for the given controller
 -- without doing a full mode change
-cardSwitchFrameBuffer :: Card -> Controller -> FrameBuffer -> PageFlipFlags -> SysRet ()
-cardSwitchFrameBuffer card ctrl fb flags =
-   switchFrameBuffer' card (controllerID ctrl) (fbID fb) flags
-
+switchFrameBuffer :: Controller -> FrameBuffer -> PageFlipFlags -> SysRet ()
+switchFrameBuffer ctrl fb flags =
+   switchFrameBuffer' (controllerCard ctrl) (controllerID ctrl) (fbID fb) flags
