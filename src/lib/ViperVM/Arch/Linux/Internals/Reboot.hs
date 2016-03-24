cde09b28b9f443809918d3b7fcfd199c83470f8a
Sylvain HENRY <hsyl20@gmail.com>
2016-03-24 18:33:14 +0100

    Doc

---
+++ src/lib/ViperVM/Arch/Linux/Internals/Reboot.hs
@@ -0,0 +1,52 @@
+module ViperVM.Arch.Linux.Internals.Reboot
+   ( PowerCommand(..)
+   , sysPower
+   )
+where
+
+import Data.Word (Word64)
+import ViperVM.Arch.Linux.ErrorCode
+import Foreign.C.String (withCString)
+import Foreign.Ptr (nullPtr)
+
+import ViperVM.Arch.Linux.Syscalls
+
+-- =============================================================
+--    From linux/include/uapi/linux/reboot.h
+-- =============================================================
+
+-- | Commands supported by the reboot syscall
+data PowerCommand
+   = PowerDisableRebootKeys     -- ^ Ctrl-Alt-Del sequence sends SIGINT to init task.
+   | PowerEnableRebootKeys      -- ^ Ctrl-Alt-Del sequence causes RESTART command.
+   | PowerHalt                  -- ^ Stop OS and give system control to ROM monitor, if any.
+   | PowerKernelExec            -- ^ Restart system using a previously loaded Linux kernel
+   | PowerOff                   -- ^ Stop OS and remove all power from system, if possible.
+   | PowerRestart               -- ^ Restart system using default command and mode.
+   | PowerRestartCommand String -- ^ Restart system using given command string.
+   | PowerSoftSuspend           -- ^ Suspend system using software suspend if compiled in.
+   deriving (Show,Eq)
+
+fromPowerCommand :: PowerCommand -> Word64
+fromPowerCommand x = case x of
+   PowerDisableRebootKeys  -> 0x00000000
+   PowerEnableRebootKeys   -> 0x89ABCDEF
+   PowerHalt               -> 0xCDEF0123
+   PowerKernelExec         -> 0x45584543
+   PowerOff                -> 0x4321FEDC
+   PowerRestart            -> 0x01234567
+   PowerRestartCommand _   -> 0xA1B2C3D4
+   PowerSoftSuspend        -> 0xD000FCE2
+
+-- | reboot syscall
+sysPower :: PowerCommand -> SysRet ()
+sysPower cmd = case cmd of
+      PowerRestartCommand cmdPath -> withCString cmdPath f
+      _                           -> f nullPtr
+   where
+      f path = onSuccessVoid (syscall_reboot magic1 magic2 cmd' path)
+      magic1 = 0xfee1dead :: Word64
+      magic2 = 0x28121969 :: Word64
+      cmd'   = fromPowerCommand cmd
+
+
--- src/lib/ViperVM/Arch/Linux/Power.hs
+++ src/lib/ViperVM/Arch/Linux/Power.hs
@@ -1,86 +1,47 @@
 module ViperVM.Arch.Linux.Power
-   ( PowerCommand(..)
-   , sysPower
-   , disableRebootKeys
+   ( disableRebootKeys
    , enableRebootKeys
    , halt
    , executeLoadedKernel
    , powerOff
    , restart
    , restartWithCommand
-   , hibernate
+   , softSuspend
    )
 where
 
-import Data.Word (Word64)
-import ViperVM.Arch.Linux.ErrorCode
-import Foreign.C.String (withCString)
-import Foreign.Ptr (nullPtr)
-
-import ViperVM.Arch.Linux.Syscalls
 import ViperVM.Arch.Linux.Error
+import ViperVM.Arch.Linux.ErrorCode
+import ViperVM.Arch.Linux.Internals.Reboot
 
-data PowerCommand
-   = PowerDisableRebootKeys
-   | PowerEnableRebootKeys
-   | PowerHalt
-   | PowerKernelExec
-   | PowerOff
-   | PowerRestart
-   | PowerRestartCommand String
-   | PowerHibernate
-   deriving (Show,Eq)
-
-fromPowerCommand :: PowerCommand -> Word64
-fromPowerCommand x = case x of
-   PowerDisableRebootKeys  -> 0x00000000
-   PowerEnableRebootKeys   -> 0x89ABCDEF
-   PowerHalt               -> 0xCDEF0123
-   PowerKernelExec         -> 0x45584543
-   PowerOff                -> 0x4321FEDC
-   PowerRestart            -> 0x01234567
-   PowerRestartCommand _   -> 0xA1B2C3D4
-   PowerHibernate          -> 0xD000FCE2
-
--- | reboot syscall
-sysPower :: PowerCommand -> SysRet ()
-sysPower cmd = case cmd of
-      PowerRestartCommand cmdPath -> withCString cmdPath f
-      _                           -> f nullPtr
-   where
-      f path = onSuccess (syscall_reboot magic1 magic2 cmd' path) (const ())
-      magic1 = 0xfee1dead :: Word64
-      magic2 = 0x28121969 :: Word64
-      cmd' = fromPowerCommand cmd
-
--- | Disable the reboot keys
+-- | Ctrl-Alt-Del sequence sends SIGINT to init task.
 disableRebootKeys :: SysRet ()
 disableRebootKeys = sysPower PowerDisableRebootKeys
 
--- | Enable the reboot keys
+-- | Ctrl-Alt-Del sequence causes RESTART command.
 enableRebootKeys :: SysRet ()
 enableRebootKeys = sysPower PowerEnableRebootKeys
 
--- | Halt the computer
+-- | Stop OS and give system control to ROM monitor, if any.
 halt :: Sys ()
 halt = sysCallAssert "Halting" $ sysPower PowerHalt
 
--- | Execute a kernel previously loaded
+-- | Restart system using a previously loaded Linux kernel
 executeLoadedKernel :: SysRet ()
 executeLoadedKernel = sysPower PowerKernelExec
 
--- | Power-off the computer
+-- | Stop OS and remove all power from system, if possible.
 powerOff :: Sys ()
 powerOff = sysCallAssert "Powering off" $ sysPower PowerOff
 
--- | Restart the computer
+-- | Restart system using default command and mode.
 restart :: Sys ()
 restart = sysCallAssert "Restarting" $ sysPower PowerRestart
 
--- | Restart the system with the given command
+-- | Restart system using given command string.
 restartWithCommand :: String -> SysRet ()
 restartWithCommand = sysPower . PowerRestartCommand
 
--- | Hibernate on disk
-hibernate :: SysRet ()
-hibernate = sysPower PowerHibernate
+-- | Suspend system using software suspend if compiled in.
+softSuspend :: SysRet ()
+softSuspend = sysPower PowerSoftSuspend
