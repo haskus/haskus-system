75164656322632a32c309aa1932cbb33b52e859c
Sylvain HENRY <hsyl20@gmail.com>
2016-03-25 16:58:06 +0100

    Add System module

---
+++ src/lib/ViperVM/System.hs
@@ -0,0 +1,12 @@
+module ViperVM.System
+   ( module X
+   )
+where
+
+import ViperVM.System.Devices  as X
+import ViperVM.System.Graphics as X
+import ViperVM.System.Input    as X
+import ViperVM.System.Process  as X
+import ViperVM.System.Sys      as X
+import ViperVM.System.System   as X
+import ViperVM.System.Terminal as X
