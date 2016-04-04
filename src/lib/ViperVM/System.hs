a1720f695f65397f4a086dfc906b586eb3738f16
Sylvain HENRY <hsyl20@gmail.com>
2016-04-04 13:24:48 +0200

    Add helpers to simplify examples

--- src/lib/ViperVM/System.hs
+++ src/lib/ViperVM/System.hs
@@ -1,5 +1,7 @@
 module ViperVM.System
    ( module X
+   , forM_
+   , forM
    )
 where
 
@@ -10,3 +12,8 @@ import ViperVM.System.Process  as X
 import ViperVM.System.Sys      as X
 import ViperVM.System.System   as X
 import ViperVM.System.Terminal as X
+
+
+-- useful helpers
+import Data.Foldable (forM_)
+import Data.Traversable (forM)
---
+++ src/lib/ViperVM/System/Power.hs
@@ -0,0 +1,6 @@
+module ViperVM.System.Power
+   ( module X
+   )
+where
+
+import ViperVM.Arch.Linux.Power as X
