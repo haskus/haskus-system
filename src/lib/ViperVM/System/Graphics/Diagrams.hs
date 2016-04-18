0b2f7fc25fd266b94d3fc867da33796bb51c87aa
Sylvain HENRY <hsyl20@gmail.com>
2016-04-18 15:41:09 +0200

    Fix renderDiagram monoid

--- src/lib/ViperVM/System/Graphics/Diagrams.hs
+++ src/lib/ViperVM/System/Graphics/Diagrams.hs
@@ -7,6 +7,7 @@ where
 import Diagrams
 import Diagrams.Backend.Rasterific
 import Codec.Picture.Types
+import Data.Monoid (Any)
 
-renderDiagram :: (TypeableFloat n,Monoid' m) => SizeSpec V2 n -> QDiagram Rasterific V2 n m -> Image PixelRGBA8
+renderDiagram :: (TypeableFloat n) => SizeSpec V2 n -> QDiagram Rasterific V2 n Any -> Image PixelRGBA8
 renderDiagram spec = renderDia Rasterific (RasterificOptions spec)
