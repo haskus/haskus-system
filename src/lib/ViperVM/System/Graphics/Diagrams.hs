e9b7cc811b58f3e68fffb9f163611ef708f25669
Sylvain HENRY <hsyl20@gmail.com>
2016-07-05 16:52:05 +0200

    Export simpler types

--- src/lib/ViperVM/System/Graphics/Diagrams.hs
+++ src/lib/ViperVM/System/Graphics/Diagrams.hs
@@ -2,6 +2,8 @@
 -- | Diagrams utilities
 module ViperVM.System.Graphics.Diagrams
    ( renderDiagram
+   , VDiagram
+   , VDiagram'
    , module Diagrams
    )
 where
@@ -11,6 +13,9 @@ import Diagrams.Backend.Rasterific
 import Codec.Picture.Types
 import Data.Monoid (Any)
 
+type VDiagram' n = QDiagram Rasterific V2 n Any
+type VDiagram    = QDiagram Rasterific V2 Float Any
+
 -- | Render a diagram into an image that can be displayed on a framebuffer
-renderDiagram :: (TypeableFloat n) => SizeSpec V2 n -> QDiagram Rasterific V2 n Any -> Image PixelRGBA8
+renderDiagram :: (TypeableFloat n) => SizeSpec V2 n -> VDiagram' n -> Image PixelRGBA8
 renderDiagram spec = renderDia Rasterific (RasterificOptions spec)
