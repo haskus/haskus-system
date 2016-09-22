1f6a684195e3e0a823ddc9eb81e636652d2112d2
Sylvain HENRY <hsyl20@gmail.com>
2016-09-22 16:21:55 +0200

    Export modules in haddock

--- src/lib/ViperVM/System.hs
+++ src/lib/ViperVM/System.hs
@@ -1,6 +1,19 @@
--- | System
+-- | System programming
+--
+-- This module reexport other modules and functions useful for system
+-- programming with ViperVM.
 module ViperVM.System
-   ( module X
+   ( module ViperVM.System.Devices
+   , module ViperVM.System.Event
+   , module ViperVM.System.Graphics
+   , module ViperVM.System.Input
+   , module ViperVM.System.Process
+   , module ViperVM.System.Sys
+   , module ViperVM.System.System
+   , module ViperVM.System.Terminal
+   , module ViperVM.System.Power
+   , module ViperVM.System.FileSystem
+   , module ViperVM.Utils.Flow
    , forM_
    , forM
    , traverse_
@@ -10,17 +23,17 @@ module ViperVM.System
    )
 where
 
-import ViperVM.System.Devices    as X
-import ViperVM.System.Event      as X
-import ViperVM.System.Graphics   as X
-import ViperVM.System.Input      as X
-import ViperVM.System.Process    as X
-import ViperVM.System.Sys        as X
-import ViperVM.System.System     as X
-import ViperVM.System.Terminal   as X
-import ViperVM.System.Power      as X
-import ViperVM.System.FileSystem as X
-import ViperVM.Utils.Flow        as X
+import ViperVM.System.Devices
+import ViperVM.System.Event
+import ViperVM.System.Graphics
+import ViperVM.System.Input
+import ViperVM.System.Process
+import ViperVM.System.Sys
+import ViperVM.System.System
+import ViperVM.System.Terminal
+import ViperVM.System.Power
+import ViperVM.System.FileSystem
+import ViperVM.Utils.Flow
 
 
 -- useful helpers
