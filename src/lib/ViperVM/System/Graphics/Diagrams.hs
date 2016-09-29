f683c58da0cbe6a9921ace30599438c6d8671e25
Sylvain HENRY <hsyl20@gmail.com>
2016-09-29 17:44:26 +0200

    Export texterific's text primitives

--- src/lib/ViperVM/System/Graphics/Diagrams.hs
+++ src/lib/ViperVM/System/Graphics/Diagrams.hs
@@ -1,10 +1,13 @@
+{-# LANGUAGE FlexibleContexts #-}
 
--- | Diagrams utilities
+-- | Diagrams utilities (specialized for the rasterific backend)
 module ViperVM.System.Graphics.Diagrams
    ( rasterizeDiagram
    , VDiagram
    , VDiagram'
    , module Diagrams
+   , text'
+   , text
    )
 where
 
@@ -12,7 +15,8 @@ where
 -- We might use Diagrams queries to handle mouse clicks, etc.
 -- http://projects.haskell.org/diagrams/blog/2015-04-30-GTK-coordinates.html
 
-import Diagrams
+import Diagrams hiding (text)
+import Diagrams.TwoD.Text (FontSlant(..),FontWeight(..),Text(..))
 import Diagrams.Backend.Rasterific
 import Codec.Picture.Types
 import Data.Monoid (Any)
@@ -24,3 +28,14 @@ type VDiagram    = QDiagram Rasterific V2 Float Any
 rasterizeDiagram :: (TypeableFloat n) => SizeSpec V2 n -> VDiagram' n -> Image PixelRGBA8
 rasterizeDiagram spec = renderDia Rasterific (RasterificOptions spec)
 
+-- | Create a primitive text diagram from the given FontSlant, FontWeight, and
+-- string, with baseline alignment, envelope and trace based on the BoundingBox
+-- of the text.
+text' :: (TypeableFloat n, Renderable (Text n) b) => FontSlant -> FontWeight -> String -> QDiagram b V2 n Any
+text' = texterific'
+
+
+-- | Create a primitive text diagram from the given string, with baseline
+-- alignment, envelope and trace based on the BoundingBox of the text. 
+text :: (TypeableFloat n, Renderable (Text n) b) => String -> QDiagram b V2 n Any
+text = texterific
