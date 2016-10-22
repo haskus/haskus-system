ee0d0232c8bf6b7fbb5e7f5cf13d188fc74e9022
Sylvain HENRY <hsyl20@gmail.com>
2016-10-22 11:14:53 +0200

    Add embedBytes/embedBuffer

--- src/lib/ViperVM/Utils/Embed.hs
+++ src/lib/ViperVM/Utils/Embed.hs
@@ -3,23 +3,35 @@
 -- | Embed data into the executable binary
 module ViperVM.Utils.Embed
    ( embedBS
+   , embedBytes
+   , embedBuffer
    , module Data.FileEmbed
    )
 where
 
-import qualified Data.ByteString as BS
-import qualified Data.ByteString.Char8 as B8
 import Data.ByteString.Unsafe (unsafePackAddressLen)
 import System.IO.Unsafe (unsafePerformIO)
 import Language.Haskell.TH
-
 import Data.FileEmbed
 
--- | Embed a bytestring into a binary
-embedBS :: BS.ByteString -> Q Exp
-embedBS bs =
-    return $ VarE 'unsafePerformIO
-      `AppE` (VarE 'unsafePackAddressLen
-      `AppE` LitE (IntegerL $ fromIntegral $ B8.length bs)
-      `AppE` LitE (StringPrimL $ BS.unpack bs))
+import ViperVM.Format.Binary.Word
+import ViperVM.Format.Binary.Buffer
 
+-- | Embed a [Word8] into a binary, return a ByteString
+embedBS :: [Word8] -> Q Exp
+embedBS bs =
+   return $ VarE 'unsafePerformIO
+     `AppE` (VarE 'unsafePackAddressLen
+     `AppE` LitE (IntegerL $ fromIntegral $ length bs)
+     `AppE` LitE (StringPrimL bs))
+
+-- | Embed a [Word8] into a binary, return a Buffer
+embedBuffer :: [Word8] -> Q Exp
+embedBuffer bs = do
+   bs' <- embedBS bs
+   return $ VarE 'bufferPackByteString
+      `AppE` bs'
+
+-- | Embed bytes in a C array, return an Addr#
+embedBytes :: [Word8] -> Q Exp
+embedBytes bs = return $ LitE (StringPrimL bs)
