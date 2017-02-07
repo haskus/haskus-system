52215491bb4f90cf623682eff948f2636ddbbccb
Sylvain Henry <sylvain@haskus.fr>
2017-02-07 15:15:50 +0100

    Doc

--- src/lib/Haskus/Arch/Linux/Internals/Input.hs
+++ src/lib/Haskus/Arch/Linux/Internals/Input.hs
@@ -100,9 +100,72 @@ import System.IO.Unsafe (unsafePerformIO)
 
 -- =============================================================
 --    From linux/include/uapi/linux/input-event-codes.h
+--    and  linux/Documentation/input/event-codes.txt
 -- =============================================================
 
 -- | Device properties and quirks
+--
+-- PropertyDirect + PropertyPointer
+-- --------------------------------
+-- The PropertyDirect property indicates that device coordinates should be
+-- directly mapped to screen coordinates (not taking into account trivial
+-- transformations, such as scaling, flipping and rotating). Non-direct input
+-- devices require non-trivial transformation, such as absolute to relative
+-- transformation for touchpads. Typical direct input devices: touchscreens,
+-- drawing tablets; non-direct devices: touchpads, mice.
+--
+-- The PropertyPointer property indicates that the device is not transposed on
+-- the screen and thus requires use of an on-screen pointer to trace user's
+-- movements.  Typical pointer devices: touchpads, tablets, mice; non-pointer
+-- device: touchscreen.
+--
+-- If neither PropertyDirect or PropertyPointer are set, the property is
+-- considered undefined and the device type should be deduced in the traditional
+-- way, using emitted event types.
+--
+-- PropertyButtonPad
+-- -----------------
+-- For touchpads where the button is placed beneath the surface, such that
+-- pressing down on the pad causes a button click, this property should be set.
+-- Common in clickpad notebooks and macbooks from 2009 and onwards.
+--
+-- Originally, the buttonpad property was coded into the bcm5974 driver version
+-- field under the name integrated button. For backwards compatibility, both
+-- methods need to be checked in userspace.
+--
+-- PropertySemiMultiTouch
+-- ----------------------
+-- Some touchpads, most common between 2008 and 2011, can detect the presence of
+-- multiple contacts without resolving the individual positions; only the number
+-- of contacts and a rectangular shape is known. For such touchpads, the semi-mt
+-- property should be set.
+--
+-- Depending on the device, the rectangle may enclose all touches, like a
+-- bounding box, or just some of them, for instance the two most recent touches.
+-- The diversity makes the rectangle of limited use, but some gestures can
+-- normally be extracted from it.
+--
+-- If PropertySemiMultiTouch is not set, the device is assumed to be a true MT
+-- device.
+--
+-- PropertyTopButtonPad
+-- --------------------
+-- Some laptops, most notably the Lenovo *40 series provide a trackstick device
+-- but do not have physical buttons associated with the trackstick device.
+-- Instead, the top area of the touchpad is marked to show visual/haptic areas
+-- for left, middle, right buttons intended to be used with the trackstick.
+--
+-- If PropertyTopButtonPad is set, userspace should emulate buttons accordingly.
+-- This property does not affect kernel behavior.
+-- The kernel does not provide button emulation for such devices but treats them
+-- as any other PropertyButtonPad device.
+--
+-- PropertyAccelerometer
+-- ---------------------
+-- Directional axes on this device (absolute and/or relative x, y, z) represent
+-- accelerometer data. All other axes retain their meaning. A device must not
+-- mix regular directional axes and accelerometer axes on the same event node.
+--
 data Property
    = PropertyNeedPointer      -- ^ needs a pointer
    | PropertyDirect           -- ^ direct input devices
@@ -115,18 +178,18 @@ data Property
 
 -- | Event types
 data EventType
-   = EventTypeSync
-   | EventTypeKey
-   | EventTypeRelative
-   | EventTypeAbsolute
-   | EventTypeMisc
-   | EventTypeSwitch
-   | EventTypeLED
-   | EventTypeSound
-   | EventTypeReplay
-   | EventTypeForceFeedback
-   | EventTypePower
-   | EventTypeForceFeedbackStatus
+   = EventTypeSync                -- ^ Marker to separate events
+   | EventTypeKey                 -- ^ State changes of keyboards, button, etc.
+   | EventTypeRelative            -- ^ Relative axis changes
+   | EventTypeAbsolute            -- ^ Absolute axis changes
+   | EventTypeMisc                -- ^ Miscellaneous
+   | EventTypeSwitch              -- ^ Binary state switch
+   | EventTypeLED                 -- ^ Turn LEDs on and off
+   | EventTypeSound               -- ^ Output sound to devices
+   | EventTypeReplay              -- ^ Auto-repeating devices
+   | EventTypeForceFeedback       -- ^ Send force-feedback to an input device
+   | EventTypePower               -- ^ Power button and switch
+   | EventTypeForceFeedbackStatus -- ^ Receive force-feedback status
    deriving (Show,Eq,Enum)
 
 instance CEnum EventType where
@@ -141,10 +204,10 @@ instance CEnum EventType where
 
 -- | Synchronization events
 data SyncEventType
-   = SyncReport
+   = SyncReport           -- ^ Separate events into packets of events occuring at the same time
    | SyncConfig
-   | SyncMultiTouchReport
-   | SyncDropped
+   | SyncMultiTouchReport -- ^ Separate touch events
+   | SyncDropped          -- ^ Buffer overrun: client should ignore all events up to the next SyncReport and query the device to obtain its current state
    deriving (Show,Eq,Enum,CEnum)
 
 -- | Key event type
