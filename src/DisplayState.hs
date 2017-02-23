7c760440cac396c0a0c295729b65667f7b5c7e68
Sylvain Henry <sylvain@haskus.fr>
2017-02-23 16:25:29 +0100

    Add DisplayState example

---
+++ src/DisplayState.hs
@@ -0,0 +1,16 @@
+import Haskus.System
+import Haskus.Arch.Linux.Graphics.State
+
+main :: IO ()
+main = runSys <| do
+   sys   <- defaultSystemInit
+   term  <- defaultTerminal
+   cards <- loadGraphicCards (systemDeviceManager sys)
+   
+   forM_ cards <| \card -> do
+      state <- readGraphicsState (graphicCardHandle card)
+               >..~!!> assertShow "Cannot read graphics state"
+
+      writeStrLn term (show state)
+
+   void powerOff
