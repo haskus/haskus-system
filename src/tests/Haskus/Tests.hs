f0a16efe98080ce66c59d7b9822c0a3a6e268d9c
Sylvain Henry <sylvain@haskus.fr>
2017-06-13 00:50:01 +0200

    Remove remaining Utils tests (now in haskus-utils package)

--- src/tests/Haskus/Tests.hs
+++ src/tests/Haskus/Tests.hs
@@ -2,15 +2,13 @@ module Haskus.Tests where
 
 import Test.Tasty
 
-import Haskus.Tests.Utils
 import Haskus.Tests.Format
 import Haskus.Tests.System
 import Haskus.Tests.Arch
 
 tests :: TestTree
 tests = testGroup "Tests" $ 
-   [ testsUtils
-   , testsFormat
+   [ testsFormat
    , testsSystem
    , testsArch
    ]
