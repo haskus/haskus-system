b0943f0901e573fc8de714bc2ab35140d6342b8d
Sylvain Henry <sylvain@haskus.fr>
2019-06-29 03:04:15 +0200

    DRM: enhance Mode display

--- haskus-system-examples/src/display-connectors/Main.hs
+++ haskus-system-examples/src/display-connectors/Main.hs
@@ -4,7 +4,9 @@
 import Haskus.System
 
 import Haskus.System.Linux.Graphics.State
+import Haskus.System.Linux.Graphics.Property
 import Haskus.System.Linux.Graphics.Object
+import Haskus.System.Linux.Graphics.Mode
 
 main :: IO ()
 main = runSys' do
@@ -13,10 +15,12 @@ main = runSys' do
    cards <- loadGraphicCards (systemDeviceManager sys)
    
    forM_ cards \card -> do
-      state <- assertE "Read graphics state"
-                  <| readGraphicsState (graphicCardHandle card)
+
+      state <- readGraphicsState (graphicCardHandle card)
+                  |> assertLogShowErrorE "Read graphics state"
 
       let conns = graphicsConnectors state
+
       when (null conns) do
          writeStrLn term "No connector found"
 
@@ -27,7 +31,12 @@ main = runSys' do
             Disconnected      -> writeStrLn term " -> disconnected"
             ConnectionUnknown -> writeStrLn term " -> unknown connection"
             Connected dev -> do
+               writeStrLn term "Modes"
                forM_ (connectedDeviceModes dev) \mode ->
-                  writeStrLn term (show mode)
+                  writeStrLn term (showMode mode)
+
+               writeStrLn term "Properties"
+               forM_ (connectedDeviceProperties dev) \prop ->
+                  writeStrLn term ("    " ++ showProperty prop)
 
    powerOff
--- haskus-system/src/lib/Haskus/System/Linux/Graphics/Mode.hs
+++ haskus-system/src/lib/Haskus/System/Linux/Graphics/Mode.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE DataKinds #-}
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE TypeApplications #-}
 
 -- | Display mode (resolution, refresh rate, etc.)
@@ -9,6 +10,7 @@ module Haskus.System.Linux.Graphics.Mode
    , ModeTypes
    , ModeFlag(..)
    , ModeFlags
+   , showMode
    -- * Low level
    , fromStructMode
    , toStructMode
@@ -16,12 +18,14 @@ module Haskus.System.Linux.Graphics.Mode
 where
 
 import Haskus.Format.Binary.BitField
+import qualified Haskus.Format.Binary.BitSet as BitSet
 import Haskus.Format.Binary.Enum
 import Haskus.Format.Binary.Word
 import Foreign.Ptr (castPtr)
 import Haskus.Format.Binary.Storable
 import Haskus.Format.String
 import Haskus.System.Linux.Internals.Graphics
+import qualified Haskus.Utils.List as List
 
 -- | Display mode
 data Mode = Mode
@@ -46,6 +50,50 @@ data Mode = Mode
    , modeType                :: !ModeTypes
    , modeName                :: !String
    } deriving (Show)
+
+-- | Show ModeFlag
+showModeFlag :: ModeFlag -> String
+showModeFlag = \case
+   ModeFlagPHSync      -> "+HSync"
+   ModeFlagNHSync      -> "-HSync"
+   ModeFlagPVSync      -> "+VSync"
+   ModeFlagNVSync      -> "-VSync"
+   ModeFlagInterlace   -> "Interlace"
+   ModeFlagDoubleScan  -> "Double-scan"
+   ModeFlagCSync       -> "CSync"
+   ModeFlagPCSync      -> "+CSync"
+   ModeFlagNCSync      -> "-CSync"
+   ModeFlagHSkew       -> "HSkew"
+   ModeFlagBroadCast   -> "Broadcast"
+   ModeFlagPixMux      -> "PixMux"
+   ModeFlagDoubleClock -> "Clock*2"
+   ModeFlagClockDiv2   -> "Clock/2"
+
+-- | Show mode
+showMode :: Mode -> String
+showMode Mode{..} = mconcat
+   [ modeName
+   , " "
+   , show modeVerticalRefresh
+   , "MHz "
+   , mconcat (List.intersperse " " (fmap showModeFlag (BitSet.elems modeFlags)))
+   , "\n\th: width ", show4 modeHorizontalDisplay
+   , " start ", show4 modeHorizontalSyncStart
+   , " end ", show4 modeHorizontalSyncEnd
+   , " total ", show4 modeHorizontalTotal
+   , " skew ", show4 modeHorizontalSkew
+   , "\n\tv: width ", show4 modeVerticalDisplay
+   , " start ", show4 modeVerticalSyncStart
+   , " end ", show4 modeVerticalSyncEnd
+   , " total ", show4 modeVerticalTotal
+   , " scan ", show4 modeVerticalScan
+   ]
+   where
+      show4 :: Show a => a -> String
+      show4 x = go (show x)
+      go x
+         | length x < 4 = go (' ':x)
+         | otherwise    = x
 
 instance Storable Mode where
    sizeOf _    = sizeOfT     @StructMode
