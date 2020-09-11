ed24aeb9d3cfc89c30e4fc58114d948269b41f20
Sylvain Henry <sylvain@haskus.fr>
2020-09-11 19:54:15 +0200

    Example: map PCM device

--- haskus-system-examples/src/sound/Main.hs
+++ haskus-system-examples/src/sound/Main.hs
@@ -9,9 +9,14 @@ import Haskus.Utils.Maybe
 import qualified Haskus.System.Linux.Internals.Sound as Snd
 import Haskus.System.Linux.Sound.Pcm
 import Haskus.System.Linux.FileSystem (close)
+import Haskus.Binary.BitSet as BitSet
+import Haskus.System.Linux.Memory
+import Foreign.ForeignPtr hiding (newForeignPtr)
+import Foreign.Concurrent (newForeignPtr)
 
 import System.FilePath
 import Data.List as List
+import qualified Data.Set as Set
 
 main :: IO ()
 main = runSys' <| do
@@ -78,8 +83,33 @@ main = runSys' <| do
       writeStrLn term ("Channel 1: " <> show chan1)
       writeStrLn term ("Channel 2: " <> show chan2)
 
+      -- FIXME: support more access modes
+      case (Set.toList . pcmConfigAccess) <$> hw_params of
+         VRight [Snd.PcmAccessMmapInterleaved] -> return ()
+         _                                     -> error "Invalid access mode"
+
       rp <- runE (Snd.ioctlPcmPrepare pcm)
       writeStrLn term ("Prepare: " <> show rp)
+
+      let sz = 1024 -- FIXME: buffer size
+      vaddr <- runE $ sysMemMap Nothing sz
+               (BitSet.fromList [ProtRead,ProtWrite])
+               (BitSet.fromList [MapShared])
+               Nothing
+               (Just (pcm, 0)) -- FIXME: handle offsets..
+                  -- -- free buffer on mapping error
+                  -- |> onE_ (runE_ (handleFreeGenericBuffer hdl (cdHandle r)))
+
+      case vaddr of
+         VLeft _     -> error "Error while mapping"
+         VRight addr -> do
+
+            -- create a foreign pointer that automatically unmaps the buffer
+            let finalizer = runE_ (sysMemUnmap addr sz)
+            fptr <- liftIO (newForeignPtr addr finalizer)
+
+            writeStrLn term ("Mmaped: " <> show addr)
+
       runE_ (close pcm)
 
    powerOff
